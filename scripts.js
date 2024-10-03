0d95368d91f6221e5156975c18873e9d4b058008
Vitor Palheta <palheta.vitor@gmail.com>
2024-10-02 23:57:11 -0300

    Capturando o evento de submit do form

--- scripts.js
+++ scripts.js
@@ -1,8 +1,38 @@
-const amout = document.getElementById("amount");
+//Cotação de moedas do dia.
+const USD = 5.44
+const EUR = 6.01
+const GBP = 7.22
+
+// Obtendo os elementos do formulário.
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 // Manipulando o input amout para receber somente números
-amout.addEventListener("input", () => {
+amount.addEventListener("input", () => {
 
   const hasCharactersRegex = /\D+/g
-  amout.value = amount.value.replace(hasCharactersRegex, "")
+  amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+// Capturando o evento de submit do formulário
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  switch(currency.value){
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+      case "EUR":
+        convertCurrency(amount.value, EUR, "€")
+        break
+        case "GBP":
+        convertCurrency(amount.value, GBP, "£")
+        break
+  }
+}
+
+// Função para converter a moeda.
+function convertCurrency(amount, price, symbol){
+  console.log(amount, price, symbol)
+}
